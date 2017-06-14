a9de977e6594974f18a86f9b285365ca26a58b96
Kirill Kotkas <grim4ik@users.noreply.github.com>
2017-06-14 13:41:44 +0300

    added esimene

---
+++ Test.java
@@ -0,0 +1,11 @@
+public class Test{
+
+	public static double kiirusedKokku(double kiirus1, double kiirus2) {
+		return  2 / (1/kiirus1 + 1/kiirus2);
+	}
+
+	public static void main(String[] args) {
+		System.out.println("Kahe kilomeetri labimise keskmine kiirus on "+kiirusedKokku(70.0, 98.0)+" km/h");
+	}
+
+}
